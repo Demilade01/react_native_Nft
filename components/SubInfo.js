36751fcbb8f9320debaa5712d6d853edef2522b9
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-03-29 15:47:49 -0700

    ...

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { SIZES, assets } from '../constants'
 
 export const NFTTitle = () => {
   return (
@@ -17,18 +18,26 @@ export const EthPrice = () => {
   )
 }
 
-export const ImageCmp = () => {
+export const ImageCmp = ({ imgUrl, index}) => {
   return (
-    <View>
-      <Text>ImageCmp</Text>
-    </View>
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font
+      }}
+    />
   )
 }
 
 export const People = () => {
   return (
-    <View>
-      <Text>People</Text>
+    <View style={{ flexDirection: "row"}}>
+      {[assets.person02, assets.person01, assets.person04].map((imgUrl, index) => (
+        <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+      ))}
     </View>
   )
 }
@@ -43,8 +52,15 @@ export const EndDate = () => {
 
 export const SubInfo = () => {
   return (
-    <View>
-      <Text>SubInfo</Text>
+    <View style={{
+      width: '100%',
+      paddingHorizontal: SIZES.font,
+      marginTop: -SIZES.extraLarge,
+      flexDirection: "row",
+      justifyContent: "space-between"
+    }}>
+      <People />
+      <EndDate />
     </View>
   )
 }
