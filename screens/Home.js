d30b805e38ff97af8b1cb3bfaafcdde20f8113e2
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-03-28 17:17:21 -0700

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import { Text, View, SafeAreaView, FlatList } from 'react-native'
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Home
